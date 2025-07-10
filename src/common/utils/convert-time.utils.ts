c2f8079a0fa4a5567c236f3f325ce5492122af29
Amirhossein Nikoo <AmirhosseinNikoo2001@gmail.com>
2025-07-10 12:50:03 +0330

    created_at return null fix

--- src/common/utils/convert-time.utils.ts
+++ src/common/utils/convert-time.utils.ts
@@ -1,7 +1,7 @@
 import * as moment from 'moment-jalaali';
 import { ValueTransformer } from "typeorm";
 export const DateToJalali: ValueTransformer = {
-    to(value: Date) { },
+    to(value: Date) { return value },
     from(value) {
         if (value) {
             return moment(value).locale('fa').format('jYYYY-jMM-jDD HH:mm:ss')
